5c2be8b0b5371eddf7bf376fe729e6b8724e52dc
Andrea Ovalle <aovalle@Andreas-MacBook-Pro.local>
2020-11-27 10:49:48 -0500

    parallax investigations

--- CS601_HW2_Ovalle/parallax.js
+++ CS601_HW2_Ovalle/parallax.js
@@ -1,18 +1,26 @@
 
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function(e) {
+    console.log(e);
+    /**
+     * What logic should control when changes happen?
+     * What values do I need to know in order to write that logic?
+     * What values are available for me to use? And in what format?
+     */
     const distance = window.scrollY;
-    document.querySelector("section").style.transform = `translateY(${distance *
-      0.5}px)`;
-    document.querySelector(
-      ".section1_1"
-    ).style.transform = `translateY(${distance * 0.3}px)`;
+    document.querySelector("section").style.transform = `translateY(${distance * 0.5}px)`;
+    document.querySelector(".section1_1").style.transform = `translateY(${distance * 0.3}px)`;
+
     setTimeout(() => {
-      document.querySelector("header")}, 800);
-      
-    document.querySelector(
-      ".section1_2"
-      ).style.transform = `translateY(${distance * 0.4}px)`;
-      setTimeout(() => {
-        document.querySelector('header')}, 300).style.transform = `translateY(${distance * 0.4}px)`;
-      });
+        debugger;
+        document.querySelector("header");
+    }, 800);
+
+    document.querySelector(".section1_2").style.transform = `translateY(${distance * 0.4}px)`;
+
+    setTimeout(() => {
+        const headerEl = document.querySelector('header');
+        if (typeof ((headerEl || {}).style || {}).transform === "string") headerEl.style.transform = `translateY(${distance * 0.4}px)`;
+        debugger;
+    }, 300);
+});
